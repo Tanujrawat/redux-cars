7a6c79e7ee9e8fb5ba7be624505aa75f1f83497c
tanuj rawat <tanujrawat448@gmail.com>
2024-07-30 11:08:48 +0530

    adding totalCost and highlight carName functionality

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeCar } from "../store";
+import '.././styles.css';
 
 function CarList() {
   const dispatch = useDispatch();
@@ -9,10 +10,15 @@ function CarList() {
   
   // we have to change from above cars to below cars because if there is something in the searchTerm then we nned to filter our cars and display them accordingly
   // be careful in using return statments 
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter((car) => 
+  const {cars, name} = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => 
        car.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    return {
+      cars: filteredCars, 
+      name: form.name,
+    }
   });
 
   const handleCarDelete = (car) => {
@@ -20,8 +26,9 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    return data
+      .filter((car) =>
+         car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, car) => acc + car.cost, 0);
+  });
+
+  // below $ is not being used for displaying value like in template string sytax with backticks
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
